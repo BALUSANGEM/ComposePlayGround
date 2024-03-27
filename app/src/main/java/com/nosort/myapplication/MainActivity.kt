ff2d7e5e240eaa38cda62af5278a874f89de3a41
BALUSANGEM <vishnubalu.sangem@gmail.com>
2024-03-27 11:51:26 +0530

    chore: add foundatoin and material3 text

--- app/src/main/java/com/nosort/myapplication/MainActivity.kt
+++ app/src/main/java/com/nosort/myapplication/MainActivity.kt
@@ -1,53 +1,30 @@
 package com.nosort.myapplication
 
-import android.graphics.DiscretePathEffect
 import android.os.Bundle
-import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.AnimationSpec
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.animateValue
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PathEffect
-import androidx.compose.ui.graphics.Shadow
-import androidx.compose.ui.graphics.StrokeJoin
-import androidx.compose.ui.graphics.TransformOrigin
-import androidx.compose.ui.graphics.drawscope.DrawStyle
-import androidx.compose.ui.graphics.drawscope.Fill
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.toComposePathEffect
-import androidx.compose.ui.text.ExperimentalTextApi
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.style.TextGeometricTransform
-import androidx.compose.ui.text.style.TextMotion
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.nosort.myapplication.ui.text.FoundationText
+import com.nosort.myapplication.ui.text.Material3Text
 import com.nosort.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -72,11 +49,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-@OptIn(ExperimentalTextApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-
+fun Greeting(name: String) {
 
     val anim = rememberInfiniteTransition(label = "hello_world_inifinite")
     val firstColor = anim.animateColor(
@@ -139,27 +113,32 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         label = "letter_spacing"
     )
 
-    Text(
-        text = "Hello \n$name!",
-        textAlign = TextAlign.Center,
-        style = TextStyle(
-            fontSize = fontSizeSpec.value.sp,
-            fontWeight = FontWeight(fontWeightSpec.value.toInt()),
-            shadow = Shadow(
-                color = Color.DarkGray,
-                offset = Offset(10f, 15f),
-                blurRadius = 10f
-            ),
-            brush = Brush.horizontalGradient(
-                0.0f to firstColor.value,
-                0.3f to secondColor.value,
-                0.7f to thirdColor.value
-            ),
-            letterSpacing = TextUnit(value = letterSpacingSpanEm.value, type = TextUnitType.Em),
-            textDecoration = TextDecoration.Underline,
-            textGeometricTransform = TextGeometricTransform(1f, skewX.value),
-        ),
-    )
+    Column {
+
+        FoundationText(
+            name,
+            fontSizeSpec.value,
+            fontWeightSpec.value,
+            firstColor.value,
+            secondColor.value,
+            thirdColor.value,
+            letterSpacingSpanEm.value,
+            skewX.value
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Material3Text(
+            name,
+            fontSizeSpec.value,
+            fontWeightSpec.value,
+            firstColor.value,
+            secondColor.value,
+            thirdColor.value,
+            letterSpacingSpanEm.value,
+            skewX.value
+        )
+    }
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/nosort/myapplication/ui/text/Material3Text.kt
@@ -0,0 +1,52 @@
+package com.nosort.myapplication.ui.text
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextGeometricTransform
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.sp
+
+@Composable
+@OptIn(ExperimentalTextApi::class)
+fun Material3Text(
+    name: String,
+    fontSizeSpec: Float,
+    fontWeightSpec: Float,
+    firstColor: Color,
+    secondColor: Color,
+    thirdColor: Color,
+    letterSpacingSpanEm: Float,
+    skewX: Float
+) {
+    Text(
+        text = "From Material3 \n$name!",
+        textAlign = TextAlign.Center,
+        style = TextStyle(
+            fontSize = fontSizeSpec.sp,
+            fontWeight = FontWeight(fontWeightSpec.toInt()),
+            shadow = Shadow(
+                color = Color.DarkGray,
+                offset = Offset(10f, 15f),
+                blurRadius = 10f
+            ),
+            brush = Brush.horizontalGradient(
+                0.0f to firstColor,
+                0.3f to secondColor,
+                0.7f to thirdColor
+            ),
+            letterSpacing = TextUnit(value = letterSpacingSpanEm, type = TextUnitType.Em),
+            textDecoration = TextDecoration.Underline,
+            textGeometricTransform = TextGeometricTransform(1f, skewX),
+        ),
+    )
+}
